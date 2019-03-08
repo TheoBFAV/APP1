c04dc88d564dff4549a55d50c69fe2a8ff8b5cd5
pastahate <48348718+pastahate@users.noreply.github.com>
2019-03-08 10:27:46 +0100

    dernier fichier uploadé+ crypteSeq

--- client-fichier_final.py
+++ client-fichier_final.py
@@ -5,8 +5,6 @@ from lib.Network import *
 # Affiche les échanges avec le serveur (false pour désactiver)
 debug_mode (True)
 
-#Hello c'est moi :)
-
 def decale (liste, decalage):
     
     """fait un décalage de César d'un message dont la liste de caractères est pris en arguments, et le decalage à effectuer aussi."""
@@ -156,7 +154,155 @@ while True:
 
         reponse=envoyerRecevoir(decale(solution, d))
 
-    #elif load==crypteSeq:
+    elif load== "load crypteSeq":
+
+        attendre_message()
+        reponse=envoyerRecevoir("depart")
+
+        liste="""Chere Alice,
+
+Tu as raison, il faut anticiper avant que nous ne soyons reperes. J'ai termine 
+dans l'urgence mon nouveau schema de chiffrement. En voici le principe :
+- durant l'encryptage, je maintiens une sequence de tous les caracteres deja 
+  rencontres, dans l'ordre
+- pour chaque caractere c a crypter, je cherche dans la sequence s'il a deja 
+  ete rencontre
+  - si c'est la premiere fois, je l'ajoute en fin de sequence et je le laisse 
+    inchange dans le message crypte
+  - sinon, je considere d qui est soit:
+    - le caractere qui precede c dans la sequence
+    - le dernier caractere de la sequence si c est le premier caractere de la 
+      sequence
+    je remplace c par d dans le message crypte et je deplace c a la fin de la 
+    sequence
+
+Je sais que tu es l'une des rares personne a pouvoir mettre en oeuvre une 
+technique complexe comme celle-ci, cela devrait nous proteger un peu plus 
+encore. Cependant, pour me montrer que tu as bien compris ma methode, il faut 
+dans un premier temps que tu me renvoies ce message crypte comme je viens de 
+te l'expliquer. Je t'enverrai alors mon prochain message. Mets ton message 
+dans l'exercice 'crypteSeq' (le mot de passe est hasta la victoria siempre).
+
+Bien a toi,
+Bob.
+
+PS: (optionnel) pour valider cet exercice sur AppoLab, il suffit de donner le 
+mot de passe de crypteSeq (cf ci-dessus) decale a l'oppose comme pour le 
+message precedent.
+
+PPS: Pour t'aider, le debut de ce message serait transforme en :
+
+> Cherh Alicr,
+> ,TurasuhuluoneuaA,frTtlfou,AtpATuuv uurqfptasqTeeqsuqysu...
+
+Et voici un exemple d'encryption sur le texte 'abcbcca'
+
+Input: abcbcca
+
+Seq:    <vide>
+Output: <vide>
+
+Char: a
+Seq:    a
+Output: a
+
+Char: b
+Seq:    ab
+Output: ab
+
+Char: c
+Seq:    abc
+Output: abc
+
+Char: b
+Seq:    acb
+Output: abca
+
+Char: c
+Seq:    abc
+Output: abcaa
+
+Char: c
+Seq:    abc
+Output: abcaab
+
+Char: a
+Seq:    bca
+Output: abcaabc
+
+Output final: abcaabc
+"""
+
+        liste=list(liste)
+
+        liste_f=liste_finale(liste)
+
+        reponse=envoyerRecevoir(''.join(liste_f))
+
+        print(reponse)
+
+        liste=list("""Cherh Alicr,
+,MalntciMeirquitnqesiM ulhypeqcsitAoMrolppslllqvrsevrmlsCndmAlmhnp,rtuudimqug ooAmgrpua,sdnv nutiinngn
+tsihetvcpd.aSq.r,iloaS
+oluusdruvanrrqcvevhmqcr'gmS,MS'thsqem'uiqtaulati,dh rS staS,s mviv
+MScvpmmmmnoMMi'ldmdldoguycumtoyMg paodleinod.qnCaa
+s'ro Sojl,iousC ebajlsSSinguovuicavnglbonbbSqlstf
+shscssndn.Nf'c.mmaN'uuCglNjpmfsntisolrfnifobveeropmsmicceuu
+aNuevrmtsonratrui rnataoamosfoMr.rciloutlsNvraetymnvltce
+pSCrSp,egtxtvpxrl,egmlysS
+exqru'tvorxslfgmMwNwmn genc,uwNewltSguwaddsaoawHpfHuvty,gjtfd.BrpvavnsmBw..tu'""")
+
+        liste_f=retour(liste)
+
+        print(''.join(liste_f))
+
+        liste="""Cher Bob,
+,Jhrsui eunqe Jtibsjt,chBuJennjsnrJealgtcceCmunmcimsogpsotmvgmvBarJoJl,mjjBoom
+rvttdssb'nhd vp
+mo.d.VilJo.aVuicsldn:t-e,d-,jj,lm r,ttissVpmpdthssdjccr''utemeuVvnneuecmmpssadJJsvpuJdhbrd,bcs:e
+irgnkdg
+qqrjkrnnve
+einsogtudodVsnhlk:ddmbodrrhoc 
+u tbukpqd,qkan,uunclq
+rqnir
+kl
+:a,anp
+so,kc ,l tllpharsrmeeupclrea,pult(oiuqoorlllcariqsqcteqqreieents(iske
+mlki
+pcsulx  ltapkyix )iVxsd-yctburpiafkmde((Jo-sroylmlxxfes  hsicti,(tsrlce'oedlc d cdqo,'-ec es tn)e
+rdc
+neaind
+tte',capc  r)iamt-ptueeiaojc,rl do-fdepfpqa,dvoc vv tnneprVrrdc ai'ie'lvl,)cn't'cpp,uums
+pr)od
+pjrrejcasdiVtpfs:ccvnhnrd,mvsnmhaaciuod,vas
+r dpcrappc ol tlr
+rrrcc rcve
+mlt
+mtr
+almrucmmlc
+e,t
+dsncfisdruhaugqa,cqgl,v p,auunrrrhraruv pcsrrcjgsnmc
+dfnraiet'rcsr
+p,nssgc sv tumttarncoaacue't
+dsncmosdruv)adc-ndpc pl tlprrpitc'ddidri'odcqcsscqcddd mdlcctn,uderpu c pidnslm,im duga,qn
+,ustmh,s
+-mvpaper'ncsvv-pnssec s
+ turorrrcc r,qcaijor-iuu'eucasdvhtu'spccpnavndag-r'rcsd,m'annoovao o-tq,c'dctcoc)ueetcnve
+odjosiauJd.B,lfdcde'x iiqgedsyp   yrhuuei-mmhu)g,tjdgpjllssduuphq)crcmemny'h-yvvpmovmiesma.fo-idqfvrsmeftepnlc-lMgpmeu.hMfe
+cMhifdsnqoMraottfdidc( MdurxtfsccdqelluimMjxxaahqMsp
+
+jc'ueq
+urtucieyjmctvlggsrdgdevnvdMss
+pejncfc-m.i-Jyd'oecoJrcgeyihl'spulmdgiemmsvlehdmdl.fghviMqpmnrlhJMhsss -.AM omg,.PSbrdc:vJmtpJh''luesm rsnatztlAtoosmzullt,nnazosnvM iyrt,d  dopemf dJzxnadzeppr.oln
+
+ly,Vvaab' Al.PtfPt:::<,,oS>.OsxmOOqqqqqqqqq>
+ycstth>.d.rr:::krz(kr.pp.OOqq,)a>> >ttxt>>>rr:::>h>(>::r:(ar>pp>OOqaq,)b>> >ttxb>>>rr:::>h>(>::t:(aaara(br>pp>OOqbqq,)c>> >tttc>>>rr:::>h>r>::ta(bbbaa:cr>pp>OOqc,(q )a>>c>ttbb>>>rr:::>h>r>::ta:ccc:b:ar>pp>OOqaq,(  )b>>c>tttb>>>rr:::>h>r>::ta(cccaa:br>pp>OOqb,q(  c)a>>b>tthb>>>rr:::>c>(>::t:(bbbrb(cr>pp>OOqc,qq  cb)a)pp)OO:Fvobs)tnc)  cb)
+"""
+        liste=list(liste)
+
+        liste_f=retour(liste)
+
+        print(''.join(liste_f))
 
     
     break
